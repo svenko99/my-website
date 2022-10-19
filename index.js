1f9875e43038816d044cdf1de05e79a95e12b6f5
svenko99 <sven.ulcar@gmail.com>
2022-10-19 16:04:55 +0200

    Modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function DaysBetweenTwoDates(date1, date2) {
+function DaysBetweenTwoDates(date) {
   var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  var firstDate = new Date(date1);
-  var secondDate = new Date(date2);
+  var firstDate = new Date(date);
+  var secondDate = new Date();
 
   var diffDays = Math.round(
     Math.abs((firstDate.getTime() - secondDate.getTime()) / oneDay)
