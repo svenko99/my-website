e464cb2fe656d77127c00a99f248b0a5c58e0f61
svenko99 <sven@Sven-MacBook-Air.local>
2023-01-12 21:57:55 +0100

    Modified Index.js to have detailed version of timer, modified HTML and CSS for this new timer

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-function DaysBetweenTwoDates(date) {
-  var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  var firstDate = new Date(date);
-  var secondDate = new Date();
+var startDate = new Date("2021-06-12");
+var currentTime = new Date();
+var diff = currentTime - startDate;
+var diffSeconds = diff / 1000;
+var diffMinutes = diffSeconds / 60;
+var diffHours = diffMinutes / 60;
+var diffDays = diffHours / 24;
 
-  var diffDays = Math.round(
-    Math.abs((firstDate.getTime() - secondDate.getTime()) / oneDay)
-  );
-  return diffDays;
+setInterval(updateTimer, 1000);
+
+function updateTimer() {
+  diffSeconds++;
+  diffMinutes = diffSeconds / 60;
+  diffHours = diffMinutes / 60;
+  diffDays = diffHours / 24;
+  var format = document.querySelector('input[name="format"]:checked').value;
+
+  if (format === "days") {
+    document.getElementById("p").textContent = Math.floor(diffDays) + " days";
+  } else {
+    var years = Math.floor(diffDays / 365);
+    var months = Math.floor((diffDays % 365) / 30);
+    var days = Math.floor((diffDays % 365) % 30);
+    var hours = Math.floor(diffHours % 24);
+    var minutes = Math.floor(diffMinutes % 60);
+    var seconds = Math.floor(diffSeconds % 60);
+
+    var timerString =
+      years +
+      " year " +
+      months +
+      " months " +
+      days +
+      " days " +
+      hours +
+      " hours " +
+      minutes +
+      " minutes " +
+      seconds +
+      " seconds";
+
+    document.getElementById("p").textContent = timerString;
+  }
 }
-
-document.getElementById("p").textContent +=
-  DaysBetweenTwoDates("2021-06-12") + " days";
