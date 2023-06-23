7f7797a39e4579dc9ce3fb058209430525fcb91a
svenko99 <107575361+svenko99@users.noreply.github.com>
2023-06-23 17:43:02 +0200

    Redesign website

---
+++ assets/js/counter.js
@@ -0,0 +1,51 @@
+const startDate = new Date("2021-06-12");
+
+function updateTimer() {
+  const currentTime = new Date();
+  const diffTime = currentTime - startDate;
+  const diffMinutes = Math.floor(diffTime / (1000 * 60));
+  const diffHours = Math.floor(diffMinutes / 60);
+  const diffDays = Math.floor(diffHours / 24);
+
+  let years = currentTime.getFullYear() - startDate.getFullYear();
+  let months = currentTime.getMonth() - startDate.getMonth();
+  let days = currentTime.getDate() - startDate.getDate();
+
+  if (days < 0) {
+    months--;
+    const daysInPrevMonth = new Date(
+      currentTime.getFullYear(),
+      currentTime.getMonth(),
+      0
+    ).getDate();
+    days = daysInPrevMonth + days;
+  }
+
+  if (months < 0) {
+    years--;
+    months = 12 + months;
+  }
+
+  const timerParts = [
+    { value: years, unit: "year" },
+    { value: months, unit: "month" },
+    { value: days, unit: "day" },
+    { value: (diffHours % 24) + 2, unit: "hour" },
+  ]
+    .filter((part) => part.value !== 0)
+    .map((part) => `${part.value} ${part.unit}${part.value !== 1 ? "s" : ""}`);
+
+  let timerString = timerParts.join(", ");
+
+  if (timerParts.length === 0) {
+    timerString = `${diffDays} day${diffDays !== 1 ? "s" : ""}`;
+  } else {
+    timerString += ` and ${diffMinutes % 60} minute${
+      diffMinutes % 60 !== 1 ? "s" : ""
+    } or ${diffDays} day${diffDays !== 1 ? "s" : ""}`;
+  }
+
+  document.getElementById("count").textContent = timerString;
+}
+
+setInterval(updateTimer, 1000);
