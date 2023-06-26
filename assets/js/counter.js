229fed8e5cfb1d615eaa5f7591b319b4e9057ac6
svenko99 <107575361+svenko99@users.noreply.github.com>
2023-06-26 21:13:54 +0200

    Add so the dark mode is the deafult option if the system setting of device are in dark mode

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -52,12 +52,13 @@ updateTimer();
 
 // Dark mode switcher
 
-const toggle = document.querySelector("#toggle");
-toggle.addEventListener("click", modeSwitch);
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-let isLight = true;
+// Check the system settings and set the initial mode accordingly
+let isLight = !prefersDarkScheme.matches;
 
-function modeSwitch() {
+// Function to toggle the mode
+function toggleMode() {
   isLight = !isLight;
   isLight ? (toggle.innerText = "🌞") : (toggle.innerText = "🌚");
   let rootElement = document.body;
@@ -72,7 +73,39 @@ function modeSwitch() {
   });
   // also change src of the image with class logo-icon
   let logoElement = document.querySelector(".logo-icon");
-  !isLight
-    ? (logoElement.src = "assets/images/dark_cap.png")
-    : (logoElement.src = "assets/images/light_cap.png");
+  isLight
+    ? (logoElement.src = "assets/images/light_cap.png")
+    : (logoElement.src = "assets/images/dark_cap.png");
 }
+
+// Function to set the default mode
+function setDefaultMode() {
+  let rootElement = document.body;
+  let mainElement = document.querySelector(".main");
+  let boxElement = document.querySelectorAll(".box");
+  let logoElement = document.querySelector(".logo-icon");
+
+  if (isLight) {
+    rootElement.classList.remove("dark-mode");
+    mainElement.classList.remove("dark-mode");
+    boxElement.forEach((element) => {
+      element.classList.remove("dark-mode");
+    });
+    logoElement.src = "assets/images/light_cap.png";
+    toggle.innerText = "🌞";
+  } else {
+    rootElement.classList.add("dark-mode");
+    mainElement.classList.add("dark-mode");
+    boxElement.forEach((element) => {
+      element.classList.add("dark-mode");
+    });
+    logoElement.src = "assets/images/dark_cap.png";
+    toggle.innerText = "🌚";
+  }
+}
+
+// Call the setDefaultMode function to set the initial mode
+setDefaultMode();
+
+// Add event listener to the toggle button
+toggle.addEventListener("click", toggleMode);
