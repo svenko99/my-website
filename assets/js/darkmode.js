f3cbd3d9a69ebfe048aa324c59e60ee6a7e55320
svenko99 <107575361+svenko99@users.noreply.github.com>
2023-08-21 11:35:23 +0200

    Add emojis to beggining of each div"

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -27,10 +27,10 @@ function toggleMode() {
     element.classList.toggle("dark-mode");
   });
   // also change src of the image with class logo-icon
-  let logoElement = document.querySelector(".logo-icon");
-  isLight
-    ? (logoElement.src = "/assets/images/light_cap.png")
-    : (logoElement.src = "/assets/images/dark_cap.png");
+  //let logoElement = document.querySelector(".logo-icon");
+  //isLight
+  //  ? (logoElement.src = "/assets/images/light_cap.png")
+  //  : (logoElement.src = "/assets/images/dark_cap.png");
 }
 
 // Function to set the default mode
@@ -48,7 +48,7 @@ function setDefaultMode() {
     boxElement.forEach((element) => {
       element.classList.remove("dark-mode");
     });
-    logoElement.src = "/assets/images/light_cap.png";
+    //logoElement.src = "/assets/images/light_cap.png";
     svg.innerHTML = `<path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="#292e3e" />
                      <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="#292e3e" />`;
   } else {
@@ -57,7 +57,7 @@ function setDefaultMode() {
     boxElement.forEach((element) => {
       element.classList.add("dark-mode");
     });
-    logoElement.src = "/assets/images/dark_cap.png";
+    //logoElement.src = "/assets/images/dark_cap.png";
     svg.innerHTML = `<path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="#fff" /> <!-- Change the fill color to white -->
                      <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="#fff" /> <!-- Change the fill color to white -->`;
   }
