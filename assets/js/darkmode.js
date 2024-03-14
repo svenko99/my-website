c913961f5c35f12e01d914caa6716fcec626e99e
svenko99 <107575361+svenko99@users.noreply.github.com>
2024-03-14 18:58:00 +0100

    Remake side UI, now the website is only in dark mode, remove the toggle mode button

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -7,6 +7,7 @@ let isLight = !prefersDarkScheme.matches;
 // Function to toggle the mode
 function toggleMode() {
   isLight = !isLight;
+  
   const toggleButton = document.getElementById("toggle");
   const svg = toggleButton.querySelector("svg");
 
@@ -40,18 +41,19 @@ function setDefaultMode() {
   let mainElement = document.querySelector(".main");
   let boxElement = document.querySelectorAll(".box");
   let logoElement = document.querySelector(".logo-icon");
-  const toggleButton = document.getElementById("toggle");
-  const svg = toggleButton.querySelector("svg");
+  //const toggleButton = document.getElementById("toggle");
+  //const svg = toggleButton.querySelector("svg");
 
-  if (isLight) {
+  //if (isLight) {
+  if (false) { // I want to set dark mode as the only option (14. 3. 2024)
     rootElement.classList.remove("dark-mode");
     mainElement.classList.remove("dark-mode");
     boxElement.forEach((element) => {
       element.classList.remove("dark-mode");
     });
     //logoElement.src = "/assets/images/light_cap.png";
-    svg.innerHTML = `<path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="#292e3e" />
-                     <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="#292e3e" />`;
+    //svg.innerHTML = `<path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z" fill="#292e3e" />
+                     //<path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" fill="#292e3e" />`;
   } else {
     rootElement.classList.add("dark-mode");
     mainElement.classList.add("dark-mode");
